2e79f0962921a2e9c2b21ff009dcb60863277442
kang <kangchihlun@gmail.com>
2021-05-09 07:31:55 +0800

    binance 間隔8小時查詢收盤價 & 費率 拿最後一筆資料的收盘时间當作下一個的開頭

--- backtests/binance-fundrate-backtest.py
+++ backtests/binance-fundrate-backtest.py
@@ -1,67 +1,69 @@
 # coding=utf-8
-import sys
-import requests
+import asyncio
+import aiohttp
 import json
-import concurrent.futures
 from datetime import datetime
 import time
-
 base_url = "https://api.binance.com/api/v3/"
 base_url_dapi = "https://dapi.binance.com/api/v1/"
 kline_req_url = base_url+"klines"
 fundrate_req_url = base_url+"fundingRate"
 
-instruments=['BTCUSDT','ETHUSDT','EOSUSDT','LTCUSDT']
-timeIntervals=['5m','15m','1h','12h']
 
+instruments=['BTC',
+    'ETH',
+    'LINK',
+    'BNB',
+    'TRX', 
+    'DOT',
+    'ADA', 
+    'EOS',
+    'LTC',
+    'BCH',
+    'XRP',
+    'ETC',
+    'FIL',
+    'EGLD',
+    'DOGE',
+    'UNI',
+    'THETA', 
+    'XLM']
 
-def combine_urls():
-    ret = []
-    now = datetime.now()
-    endTime = str(int(time.mktime(now.timetuple())*1e3))
-    for ins in instruments:
-        for itv in timeIntervals:
-            _url = kline_req_url+'?symbol='+ins+'&interval='+itv+'&endTime='+endTime+'&limit=1000'
-            ret.append({'path':_url,'instrument':ins,'interval':itv})
-    return ret
-
-def load_url(url, timeout):
+async def request(session,url):
     headers = {'content-type': 'application/json'}
-    return requests.get(url, timeout = timeout)
+    async with session.get(url,headers=headers) as res:
+        return await res.text()
 
-def combine_records(url,kline):
-    ins=url['instrument']
-    interval=url['interval']
-    
-    mongoclient = MongoClient( mongoclient_uri + "/binance")
-    db = mongoclient['binance']
-    collection = db[ ins+'-'+interval ]
-    updatecmd = "collection.update_one("
-    updatecmd += "{'timestamp':'"+str(kline[0])+"'},"
-    updatecmd += "{'$setOnInsert':{"
-    updatecmd += "'open':'"+str(kline[1])+"',"
-    updatecmd += "'high':'"+str(kline[2])+"',"
-    updatecmd += "'low':'"+str(kline[3])+"',"
-    updatecmd += "'close':'"+str(kline[4])+"',"
-    updatecmd += "'vol':'"+str(kline[5])+"',"
+async def collectdata():    
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        now = datetime.now()
+        itv='8h'
+        startTime = 1483228800000 # 2017/1/1
+        endTime = int(time.mktime(now.timetuple())*1e3)
+        retdict = {}
         
-    updatecmd += '}}, upsert = True)'
-    print(updatecmd) 
-    exec(updatecmd)    
-
-
-def fetch_kline():
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        resp_err=0
-        future_to_url = {executor.submit(load_url, url['path'], 10): url for url in combine_urls()}
-        for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
-            try:
-                jsonObj = json.loads(future.result().text) 
-                for kline in jsonObj:
-                    combine_records(url,kline)
-            except Exception as exc:
-                resp_err = resp_err + 1
+        for ins in instruments:
+            retdict[ins] = {}
+        for ins in instruments:
+            t = startTime
+            while(t <= endTime):
+                _url = kline_req_url+'?symbol='+ins+'USDT&interval='+itv+'&startTime='+str(t)+'&limit=1000'
+                #print(_url)
+                ret = await request(session,_url)
+                arrobj = json.loads(ret)
+                
+                for kbar in arrobj:
+                    ktime = kbar[0]
+                    if(not ktime  in retdict[ins]):
+                        retdict[ins][ktime] = [float(kbar[4])]
+                    
+                t = arrobj[len(arrobj)-1][6] # 拿最後一筆資料的收盘时间當作下一個的開頭
+        return retdict
+    
+async def backtest():
+    klines = await collectdata()
+    print(klines)
+    print('done')
 
 if __name__ == "__main__":
-    fetch_kline()
+    asyncio.run(backtest())
