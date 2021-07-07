38b3cc7651e8e9b3d6221c3bb8c4f9012eb754ab
kang <kangchihlun@gmail.com>
2021-07-08 06:37:43 +0800

    2021 7 月新版績效報表重填

---
+++ backtests/gsheet-rpt-recalc-fillup.py
@@ -0,0 +1,84 @@
+# coding=utf-8
+import asyncio
+import aiohttp
+import json
+from datetime import datetime
+import gspread
+import dateutil.parser
+import time
+import codecs
+import math
+
+# gsheet usage
+# https://docs.gspread.org/en/latest/user-guide.html#opening-a-spreadsheet
+
+# google gspread
+gc = gspread.service_account()
+
+# change this
+sheetId = '1DPSTYQ0eGGk0R9oZLt-rvVwH_RccEV9luDbCttEvMB0'
+row_start = 2
+row_end = 3000
+
+async def recalc_fills():
+    
+    sh = gc.open_by_key(sheetId)
+    sheetID = sh.id
+    # 日績效
+    worksheet = sh.get_worksheet(1)
+    # 初始本金
+    initialCapital = float( worksheet.acell('B'+str(row_start)).value )
+    # 最後算完的儲存格
+    rows = []
+    
+    prvcap = initialCapital
+    hh = -99999
+    longestHHTimes = -999
+    hhCounter = 0
+    prvnet = 0
+    for k in range(row_start+1,row_end):
+        rownum = 'B'+str(k)
+        num = None
+        try:num = worksheet.acell('B'+str(k)).value
+        except:pass
+        if(num == None):break
+        curcapital = float(num)
+        net = curcapital - initialCapital
+        dayProfitPerc = (net / prvcap)*100.0
+        dayProfitPerc_fix2 = str( float("{:.2f}".format(dayProfitPerc)) )+'%'
+        
+        
+        ret_perc = (net / initialCapital)*100.0
+        ret_perc_fix2 = str( float("{:.2f}".format(ret_perc)) )+'%'
+        
+        
+        hhtext = u'創新高'
+        hhrec = ''
+        if(net>prvnet):
+            if(net > hh):
+                hh = net
+            hhCounter+=1
+            hhrec = dayProfitPerc_fix2
+            if(hhCounter > longestHHTimes):
+                longestHHTimes = hhCounter
+                
+        elif(net<prvnet):
+            hhCounter = 0
+            
+        dd = min(0,net-hh)
+        dd_perc = (dd / prvcap)*100.0
+        dd_perc_fix2 = str( float("{:.2f}".format(dd_perc)) )+'%'
+    
+        if(net<prvnet):
+            hhtext = dd_perc_fix2
+        
+        newrow = [curcapital,net,dayProfitPerc_fix2,ret_perc_fix2,dd_perc_fix2,hhtext,hhrec,hhCounter]
+        print(newrow)
+        
+        prvcap = curcapital
+        prvnet = net
+        
+    print('done')
+
+if __name__ == "__main__":
+    asyncio.run(recalc_fills())
